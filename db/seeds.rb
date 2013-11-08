79aac6041c949f07224abcd0f35b8382db99f7f1
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-11-07 19:57:51 -0600

    created seed data for accounts, categories and collaborations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+30.times do
+  Account.create  first_name: Faker::Name.first_name,
+                  last_name: Faker::Name.last_name,
+                  email: Faker::Internet.email,
+                  organization: Faker::Company.name,
+                  city: Faker::Address.city,
+                  country: Faker::Address.country
+end
+
+
+Category.create name: "Not for profit"
+Category.create name: "Small business"
+Category.create name: "Social enterprise"
+Category.create name: "Special interest group"
+
+
+30.times do
+  Collaboration.created developer_id: rand(1..20),
+                        project_id: rand(1..10),
+                        role_id: rand(1..3)
+end
+
