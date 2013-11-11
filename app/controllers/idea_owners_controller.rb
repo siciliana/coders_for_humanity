3ad97511930e5b1c854aac777aba3ed29fced122
Elizabeth Engelman <elizabethengelman@Elizabeths-MacBook-Pro.local>
2013-11-11 14:36:53 -0600

    added edit idea_owner profile functionality

--- app/controllers/idea_owners_controller.rb
+++ app/controllers/idea_owners_controller.rb
@@ -9,6 +9,16 @@ class IdeaOwnersController < ApplicationController
   end
 
   def edit
+  	@user = IdeaOwner.find_by_id(current_user.id)
+  	@account = @user.account
+  end
+
+  def update
+  	@user = IdeaOwner.find_by_id(current_user.id)
+  	@user.update_attributes(params[:idea_owner])
+    @account = @user.account
+  	@account.update_attributes(params[:account])
+  	redirect_to user_path
   end
 
 
