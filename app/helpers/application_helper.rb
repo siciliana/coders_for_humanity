86ddd5915d2d5e2898b030c63f6150b9d853be94
Luis Perez <lperez111@gmail.com>
2013-11-10 19:58:55 -0600

    added partial for showing project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,6 @@ module ApplicationHelper
     @current_user ||= IdeaOwner.find_by_id(session[:idea_owner_id]) || Developer.find_by_id(params[:developer_id])
   end
 
-  def name
-    @name = "#{current_user.first_name} #{current_user.last_name}"
-  end
-
   def developer?
     current_user.is_a? Developer
   end
