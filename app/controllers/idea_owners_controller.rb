570b0afc5b6cae146615db372460b221251c3b9f
cdcueva <msh345@gmail.com>
2013-11-10 21:27:24 -0600

    wired all links and logic to controller for dashboard

--- app/controllers/idea_owners_controller.rb
+++ app/controllers/idea_owners_controller.rb
@@ -1,6 +1,15 @@
 class IdeaOwnersController < ApplicationController
 
+  include ApplicationHelper 
+
   def show
+    @project_title = current_user.project.title
+    @project_description = current_user.project.description
+    @project_status = current_user.project.status
   end
+
+  def edit
+  end
+
 
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,24 @@
 class ProjectsController < ApplicationController
   protect_from_forgery
 
+  include ApplicationHelper 
+
   def index
     @project = Project.find_by_id(params[:id])
   end
 
   def show
-    @project = Project.find_by_id(params[:id])
+    if current_user
+      @project = current_user.project
+    else
+      @project = Project.find_by_id(params[:id])
+    end
+
     @category = Project.find_by_id(1).category.name
+  end
+
+  def edit
+    @project = current_user.project 
   end
 
 end
