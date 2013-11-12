fa5ff353aa4729daade4d4b7bf43e79ab926da46
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-11-12 08:24:01 -0600

    renamed users_helper_spec to idea_owners_helper_spec

--- app/assets/javascripts/wizard.js
+++ app/assets/javascripts/wizard.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#rootwizard').bootstrapWizard({
     'tabClass': 'bwizard-steps',
+
     onTabClick: function(tab, navigation, index) {
       return false;
     },
