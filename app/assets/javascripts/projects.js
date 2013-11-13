5760b7cd4c86e340c46b224b103d89ce4143ce8f
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-13 14:42:45 -0600

    shortened title length and added ... to the end

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   $("#tab-navigation").tab();
+
+    $(".single-title").each(function(i){
+    len=$(this).text().length;
+    if(len>60)
+    {
+      $(this).find('a').text($(this).text().substr(0,60)+'...');
+    }
+  });
 });
