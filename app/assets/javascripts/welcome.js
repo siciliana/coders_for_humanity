7c90bd5d5b7467b4c40a8caaa2bf80ee5e4dbe71
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-12 20:40:48 -0600

    homepage video is cnentered and buttons arent breaking!!!!!!

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,22 +3,22 @@ $(document).ready(function(){
       $('html, body').animate({
           scrollTop: $("#creators").offset().top
       }, 1000);
-  })
+  });
   $("#dev_button").click(function() {
       $('html, body').animate({
           scrollTop: $("#developers").offset().top
       }, 1000);
-  })
+  });
 
   $(".about_us_navbar").click(function() {
     $('html, body').animate({
         scrollTop: $("#about_us").offset().top
     }, 1000);
-  })
+  });
 
   $(".projects_navbar").click(function() {
     $('html, body').animate({
         scrollTop: $("#featured").offset().top
     }, 1000);
-  })
+  });
 });
